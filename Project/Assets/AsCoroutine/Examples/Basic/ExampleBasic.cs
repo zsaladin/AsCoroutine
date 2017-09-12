0c31406a424f92f037a2cb36d97b17cf54c595e6
Daehee Kim <zsaladinz@gmail.com>
2017-09-12 12:06:17 +0900

    ExampleBasic : Add some samples related with Start and Stop

--- Project/Assets/AsCoroutine/Examples/Basic/ExampleBasic.cs
+++ Project/Assets/AsCoroutine/Examples/Basic/ExampleBasic.cs
@@ -34,13 +34,41 @@ namespace AsCoroutine.Example
                 .Start(this);
         }
 
+        private void Sample3()
+        {
+            Cooperator cooperator = this.AsCoroutine().Action(() => Debug.Log("Sample3")).Repeat(() => true).Start(this);
+            this.AsCoroutine()
+                .YieldWaitForSeconds(2f)
+                .Action(() => cooperator.Stop())
+                .Action(() => Debug.Log("Stop"))
+                .Start(this);
+        }
+
+        private void Sample4()
+        {
+            Cooperator cooperator = this.AsCoroutine().Action(() => Debug.Log("Sample4")).Repeat(() => true);
+            StartCoroutine(cooperator);
+
+            this.AsCoroutine()
+                .YieldWaitForSeconds(2f)
+                .Action(() => StopCoroutine(cooperator))
+                .Action(() => Debug.Log("Stop"))
+                .Start(this);
+        }
+
         private void OnGUI()
         {
-            if (GUI.Button(GetRect(1, 2), "Sample1"))
+            if (GUI.Button(GetRect(1, 4), "Sample1"))
                 Sample1();
 
-            if (GUI.Button(GetRect(2, 2), "Sample2"))
+            if (GUI.Button(GetRect(2, 4), "Sample2"))
                 Sample2();
+
+			if (GUI.Button(GetRect(3, 4), "Sample3"))
+				Sample3();
+
+			if (GUI.Button(GetRect(4, 4), "Sample4"))
+				Sample4();
         }
 
         private Rect GetRect(int order, int totalOrder)
