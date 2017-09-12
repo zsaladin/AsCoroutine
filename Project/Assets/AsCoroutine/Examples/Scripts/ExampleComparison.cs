fab45533778dd36560630f7f64d88a90e7a83607
Daehee Kim <zsaladinz@gmail.com>
2017-09-12 21:28:44 +0900

    ExampleComparison : Add some example

--- Project/Assets/AsCoroutine/Examples/Scripts/ExampleComparison.cs
+++ Project/Assets/AsCoroutine/Examples/Scripts/ExampleComparison.cs
@@ -29,26 +29,53 @@ namespace AsCoroutine.Example
 				.Action(() => Debug.Log("Example2"))
 				.Start(this);
 
-			this.AsCoroutine().YieldCoroutine(Example2Coroutine());
+			this.AsCoroutine().YieldCoroutine(Example2Coroutine()).Start(this);
         }
 
 		private IEnumerator Example2Coroutine()
 		{
+			yield return Random.value < 0.5f ?  new WaitForSeconds(1f) : new WaitForSeconds(2f);
+			Debug.Log("Example2Coroutine");
+		}
+
+        private void Example3()
+        {
+            StartCoroutine(Example3Coroutine());
+
+			this.AsCoroutine()
+				.YieldAction<object>(() => 
+                {
+                    if (Random.value < 0.5f)
+                        return new WaitForSeconds(1f);
+                    else
+                        return new WaitForSecondsRealtime(2f);
+                })
+				.Action(() => Debug.Log("Example3"))
+				.Start(this);
+
+			this.AsCoroutine().YieldCoroutine(Example3Coroutine()).Start(this);
+        }
+
+		private IEnumerator Example3Coroutine()
+		{
 			if (Random.value < 0.5f)
 				yield return new WaitForSeconds(1f);
 			else
-				yield return new WaitForSeconds(2f);
+				yield return new WaitForSecondsRealtime(2f);
 
-			Debug.Log("Example2Coroutine");
+			Debug.Log("Example3Coroutine");
 		}
 
         private void OnGUI()
         {
-            if (GUI.Button(GetRect(1, 2), "Example1"))
+            if (GUI.Button(GetRect(1, 3), "Example1"))
                 Example1();
 
-            if (GUI.Button(GetRect(2, 2), "Example2"))
+            if (GUI.Button(GetRect(2, 3), "Example2"))
                 Example2();
+
+            if (GUI.Button(GetRect(3, 3), "Example3"))
+                Example3();
         }
 
         private Rect GetRect(int order, int totalOrder)
